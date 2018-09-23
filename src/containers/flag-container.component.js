c4e0aeec803b684661ffc9c7f6b6ac18a4d42001
Michał Worwąg <michalwo@gmail.com>
2018-09-23 17:37:48 +0200

    Task finished

--- src/containers/flag-container.component.js
+++ src/containers/flag-container.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CountryFlagList from '../presentational/flag-list.component';
-import { getCountries, searchCountries } from '../actions/actions-countries';
+import { getCountries, searchCountries, deleteCountry } from '../actions/actions-countries';
 import '../country.css';
 
 class CountryFlagContainer extends Component {
@@ -16,15 +16,19 @@ class CountryFlagContainer extends Component {
 
     search(event) {
         this.props.dispatch(searchCountries(event.target.value));
-}
+    }
+
+    deleteCountry(id) {
+        this.props.dispatch(deleteCountry(id));
+    }
 
     render() {
         return (
             <div>
                 <div className="search text-center">
-                    <input type="text" onChange={this.search.bind(this)}/>
+                    <input type="text" onChange={this.search.bind(this)} placeholder='Search Country'/>
                 </div>
-                <CountryFlagList countries={this.props.visibleCountries} />
+                <CountryFlagList countries={this.props.visibleCountries} deleteCountry={this.deleteCountry.bind(this)} />
             </div>
         )
     }
